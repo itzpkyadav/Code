c5231d9df6b1da4bb6c19527a53ccd7a08609341
kumarmohit9648 <kumarmohit9648@gmail.com>
2020-03-04 21:13:06 +0530

    add landscape layout

--- app/src/main/java/com/ahead/code/ui/ScheduleActivity.java
+++ app/src/main/java/com/ahead/code/ui/ScheduleActivity.java
@@ -36,7 +36,6 @@ public class ScheduleActivity extends AppCompatActivity implements View.OnClickL
     private Intent intent;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
