1e695d371cb01739e6f284a861bcfbf7cb74c2d4
Digvijay Singh <digvijay.singh@aheadsofttech.com>
2020-03-04 15:29:59 +0530

    optimize imports

--- app/src/main/java/com/ahead/code/ui/MainActivity.java
+++ app/src/main/java/com/ahead/code/ui/MainActivity.java
@@ -5,10 +5,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
